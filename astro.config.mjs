5e09a83d3a202d04d466be8e2d6defb98d438851
dorian2023 <doriangonzalez2018@gmail.com>
2024-06-07 06:42:28 -0700

    muchos cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(),icon()]
+    integrations: [tailwind(),icon()],
+   
 });
