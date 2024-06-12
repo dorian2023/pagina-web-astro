20565d81fcb3cb401fec2943d299e427826937d2
dorian2023 <doriangonzalez2018@gmail.com>
2024-06-12 16:25:30 -0400

    cambios de stylos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,12 +3,17 @@ import animations from 'tailwindcss-animated'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content:['./src/**/*.{astro,html,js.jsx,md,mdx,svelte,ts,tsx,vue}'],
+    content:['./src/**/*.{astro,html,js.jsx,md,mdx,svelte,ts,tsx,vue}',
+    "./node_modules/tw-elements/js/**/*.js"
+    ],
     theme: {
         extend: {},
     },
     plugins: [
         animations1,
-        animations
+        animations,
+        require("tw-elements/plugin.cjs"),
+        
     ],
+    darkMode: "class"
 }
