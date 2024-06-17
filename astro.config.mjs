fa129823441f3afc2d67a793c7adf78a82a45745
dorian2023 <doriangonzalez2018@gmail.com>
2024-06-17 11:50:40 -0400

    camnbio en astroo.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), preact()]
+  integrations: [tailwind(), icon(), preact()],
+  buildOptions: {
+    out: "dist" // Carpeta de salida
+  }
 });
